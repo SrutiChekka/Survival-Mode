451ea683c38d0a02835a8341f32289e0462c101e
SrutiChekka <SrutiChekka@users.noreply.github.com>
2021-06-25 00:10:22 +0530

    Add Changes

--- Survival-Mode/Assets/Scripts/ThirdPersonController.cs
+++ Survival-Mode/Assets/Scripts/ThirdPersonController.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class ThirdPersonController : MonoBehaviour
 {
-    Rigidbody myRigidbody;
 
     public CharacterController characterController;
     public Transform cam;
@@ -15,28 +13,18 @@ public class Player : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
-    public Transform bulletPos;
-    public BulletBehaviour bullet;
+
+
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
-        AimTowardsMouse();
-        Shoot();
-    }
-
-    private void Shoot()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Instantiate(bullet, bulletPos.position, Quaternion.identity);
-        }
     }
 
     private void Movement()
@@ -45,7 +33,7 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         Vector3 playerDirection = new Vector3(horizontal, 0, vertical).normalized;
 
-        if(playerDirection.magnitude >= 0.1f)
+        if (playerDirection.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(playerDirection.x, playerDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -53,18 +41,6 @@ public class Player : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             characterController.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
-        }
-    }
-
-    private void AimTowardsMouse()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out var hitInfo))
-        {
-            var direction = hitInfo.point - transform.position;
-            direction.y = 0;
-            direction.Normalize();
-            transform.forward = direction;
         }
     }
 }
